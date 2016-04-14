26e92cc1b8a1efbd6043dc35bacaecab0907a69e
puterakahfi <puterakahfi@gmail.com>
2016-04-14 10:20:01 +0700

    added sh file for copy vim from home directory

---
+++ cpvim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cp -R ~/.vim  /media/WEBSITE/puterakahfi/my-vim/
+sudo cp ~/.vimrc  /media/WEBSITE/puterakahfi/my-vim/
+sudo rm -rf /media/WEBSITE/puterakahfi/myvim/.vim/backup_files/
+sudo rm -rf /media/WEBSITE/puterakahfi/myvim/.vim/undo_files/
+sudo rm -rf /media/WEBSITE/puterakahfi/myvim/.vim/swap_files/
+sudo rm -rf /media/WEBSITE/puterakahfi/myvim/.vim/bundle.bak
